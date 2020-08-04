9137bad47e3f6229762c0f808bc01af643b3d1b2
Ralf Münninghoff <ralf@muenninghoff.info>
2020-08-04 15:57:35 +0200

    New feature added

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-    std::cout << "Hello World    !\n";
+    std::cout << "Hello World!\n";
+    std::cout << "New Feature!\n";
 }
 
 // Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
